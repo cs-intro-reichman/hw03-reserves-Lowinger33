74e3b945db90631a874483bd19cf604366c8399d
Daniel Lowinger <daniel.lowinger@post.runi.ac.il>
2024-04-11 17:04:30 +0300

    fix 4

--- Calendar.java
+++ Calendar.java
@@ -1,47 +1,41 @@
 /**
  * Prints the calendars of all the years in the 20th century.
  */
-public class Calendar1 {
+public class Calendar {
 	// Starting the calendar on 1/1/1900
 	static int dayOfMonth = 1;
 	static int month = 1;
 	static int year = 1900;
 	static int dayOfWeek = 2; // 1.1.1900 was a Monday
-	static int nDaysInMonth = 31; // Number of days in January
 
-	/**
-	 * Prints the calendars of all the years in the 20th century. Also prints the
-	 * number of Sundays that occured on the first day of the month during this
-	 * period.
-	 */
 	public static void main(String args[]) {
-		// Advances the date and the day-of-the-week from 1/1/1900 till 31/12/1999,
-		// inclusive.
-		// Prints each date dd/mm/yyyy in a separate line. If the day is a Sunday,
-		// prints "Sunday".
-		// The following variable, used for debugging purposes, counts how many days
-		// were advanced so far.
-		int debugDaysCounter = 0;
-		int special = 0;
+		int theyear = Integer.parseInt(args[0]);
+		int run = (theyear - 1900) * 365;
+		int i = 1900;
+		while (i < theyear) {
+			if (isLeapYear(i)) {
+				run++;
+			}
+			i++;
+		}
+		int j = 1;
+		while (j <= run) {
+			advance();
+			j++;
+		}
+		int save = theyear;
 
-		while (dayOfMonth <= nDaysInMonth(month, year)) {
+		while (theyear == save) {
 			System.out.print(dayOfMonth + "/" + month + "/" + year);
 			if (dayOfWeek == 1) {
 				System.out.print(" Sunday");
 			}
 			System.out.println();
-			if (dayOfMonth == 1 && dayOfWeek == 1) {
-				special++;
+			if (dayOfMonth == 31 && month == 12) {
+				theyear++;
 			}
 			advance();
-			debugDaysCounter++;
-			//// If you want to stop the loop after n days, replace the condition of the
-			//// if statement with the condition (debugDaysCounter == n)
-			if (debugDaysCounter == 1000) {
-				break;
-			}
 		}
-		System.out.println("During the 20th century, 172 Sundays fell on the first day of the month");
 	}
 
 	// Advances the date (day, month, year) and the day-of-the-week.
@@ -54,7 +48,6 @@ public class Calendar1 {
 		} else {
 			dayOfWeek = 1;
 		}
-
 		if (dayOfMonth < nDaysInMonth(month, year)) {
 			dayOfMonth++;
 		} else {
